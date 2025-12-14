cc09559dea8709b5d49cd00135a14149ceae4b98
Alex Vondrak <ajvondrak@gmail.com>
2025-12-13 18:07:18 -0800

    implement phone words search
    The code isn't necessarily the cleanest, but it's also cleaner than the
    overcomplicated representations I was using in drafts of this. This is
    the first one that seems to actually work, so committing as-is.

--- src/main.rs
+++ src/main.rs
@@ -46,6 +46,64 @@ impl Trie {
         }
         self.nodes[id].terminal
     }
+
+    fn phone_words(&self, phone_number: &str) {
+        let digits: Vec<_> = phone_number.chars().collect();
+        let mut stack: Vec<(String, NodeId, usize)> = Vec::new();
+        stack.push((String::new(), 0, 0));
+
+        while let Some((phone_word, id, d)) = stack.pop() {
+            if d == digits.len() {
+                if self.nodes[id].terminal {
+                    println!("{}", phone_word);
+                }
+                continue;
+            }
+            let digit = digits[d];
+            for letter in t9_letters(digit) {
+                if let Some(&next_id) = self.nodes[id].edges.get(letter) {
+                    let mut next_phone_word = phone_word.clone();
+                    next_phone_word.push(*letter);
+                    stack.push((next_phone_word, next_id, d + 1));
+                }
+                if self.nodes[id].terminal
+                    && let Some(&next_id) = self.nodes[0].edges.get(letter)
+                {
+                    let mut next_phone_word = phone_word.clone();
+                    next_phone_word.push(' ');
+                    next_phone_word.push(*letter);
+                    stack.push((next_phone_word, next_id, d + 1));
+                }
+            }
+        }
+    }
+}
+
+static ONE: &[char] = &[];
+static ABC: &[char] = &['a', 'b', 'c'];
+static DEF: &[char] = &['d', 'e', 'f'];
+static GHI: &[char] = &['g', 'h', 'i'];
+static JKL: &[char] = &['j', 'k', 'l'];
+static MNO: &[char] = &['m', 'n', 'o'];
+static PQRS: &[char] = &['p', 'q', 'r', 's'];
+static TUV: &[char] = &['t', 'u', 'v'];
+static WXYZ: &[char] = &['w', 'x', 'y', 'z'];
+static ZERO: &[char] = &[];
+
+fn t9_letters(digit: char) -> &'static [char] {
+    match digit {
+        '1' => ONE,
+        '2' => ABC,
+        '3' => DEF,
+        '4' => GHI,
+        '5' => JKL,
+        '6' => MNO,
+        '7' => PQRS,
+        '8' => TUV,
+        '9' => WXYZ,
+        '0' => ZERO,
+        _ => panic!("not a digit: {:?}", digit),
+    }
 }
 
 #[cfg(test)]
@@ -81,12 +139,10 @@ fn main() {
 
     let mut trie = Trie::new();
     for word in reader.lines().map_while(Result::ok) {
-        if word.chars().all(|c| c.is_alphabetic() && c.is_lowercase()) {
+        if word.len() > 1 && word.chars().all(|c| c.is_alphabetic() && c.is_lowercase()) {
             trie.insert(&word);
         };
     }
 
-    dbg!(trie.contains("dog"));
-    dbg!(trie.contains("cat"));
-    dbg!(trie.contains("catdog"));
+    trie.phone_words("8737878"); // "use rust", but also some others like "us erupt", "user tst"
 }
